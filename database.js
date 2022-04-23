903ec41afcb4039057e7d7df1a801cc3d9b8862f
Johan Rodriguez <jorodriguezal@unal.edu.co>
2022-04-23 16:22:13 -0500

    Conexión a Heroku

--- database.js
+++ database.js
@@ -1,11 +1,14 @@
 const { Client, Pool } = require('pg')
 
 const client = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
+    user: 'uwwxvblzcigqja',
+    host: 'ec2-23-20-224-166.compute-1.amazonaws.com',
+    database: 'd8u9dot11hatss',
+    password: 'c44db1ce5d4d5415659ce7452d1d75eb32853826cc278798e8b6c60d2280916f',
     port: 5432,
+    ssl:{
+        rejectUnauthorized:false,
+    }
 };
 
 const pool = new Pool(client);

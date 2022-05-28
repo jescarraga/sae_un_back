a41db81e4dba7a0c791a90044ec9bb967d7c9b6c
yeison <ycuervob@unal.edu.co>
2022-05-28 04:24:40 -0500

    post observaciones

--- routes/observaciones.js
+++ routes/observaciones.js
@@ -23,9 +23,29 @@ observacionesRouter.route('/observaciones')
     .delete(sendNull);
 
 
+
 async function docenteR(req, res, next) {
-    res.send({ status: "Estamos trabajando para tener esta chingadera funcionando"});
-    
+    var request = req.body;
+    var res1 = await queryCreator(`SELECT codigo FROM public.programas_curriculares;`);
+    var departaments = res1.rows.map((r) => { return r.id_departamento });
+
+    if (departaments.includes(+request.id_departamento)) {
+
+        var insertUsuario = await queryCreator(
+            `CALL public.insertobservacion(':documento_docente',':documento_estudiante',':codigo_plan,:observacion');`
+        );
+
+        if (!insertUsuario.command) {
+            var envio = { status: "No es posible ingresar un usuario con esos datos: " + insertUsuario };
+            return res.send(envio);
+        } else {
+            var resp = { status: true };
+            return res.send(resp);
+        }
+
+    } else {
+        res.send({ status: "tipo usuario: " + request.id_tipo_usuario + " o departamento: " + request.id_departamento + " -> no validos" });
+    }
 }
 
 

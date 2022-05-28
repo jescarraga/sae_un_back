2c648e20b1e926b08ab6d72ff70270db999e73c2
yeison <ycuervob@unal.edu.co>
2022-05-28 05:19:11 -0500

    post y get hechos, falta lo de obtener las observaciones echas por un profesor

--- routes/observaciones.js
+++ routes/observaciones.js
@@ -13,11 +13,11 @@ observacionesRouter.route('/observaciones')
     })
     .post(docenteR)
     .get(async (req, res, next) => {
-        var selectEstudiante = await queryCreator(
+        var selectEstudiantes = await queryCreator(
             `select * from public.select_estudiantes_de_docente('${req.query.documento}');`
         );
 
-        res.send(selectEstudiante.rows.map((r) => { return [r.nombres, r.apellidos, r.documento_estudiante, r.codigo_plan, r.observacion] }));
+        res.send(selectEstudiantes.rows.map((r) => { return [r.nombres, r.apellidos, r.documento_estudiante, r.codigo_plan, r.nombre_programa_curricular] }));
     })
     .put(sendNull)
     .delete(sendNull);
@@ -26,26 +26,37 @@ observacionesRouter.route('/observaciones')
 
 async function docenteR(req, res, next) {
     var request = req.body;
+    var res1 = await queryCreator(`select codigo_plan FROM public.tutores where documento_estudiante = '${request.documento_estudiante}';`);
+    var codigos_estudiante = res1.rows.map((r) => { return r.codigo_plan });
 
+    if (codigos_estudiante.includes(+request.codigo_plan)) {
+        var insertobservacion = await queryCreator(
+            `CALL public.insertobservacion(
+                '${request.documento_docente}',
+                '${request.documento_estudiante}',
+                ${request.codigo_plan},
+                '${request.observacion}');`
+        );
 
-    var insertobservacion = await queryCreator(
-        `CALL public.insertobservacion(
-            '${request.documento_docente}',
-            '${request.documento_estudiante}',
-            ${request.codigo_plan},
-            '${request.observacion}');`
-    );
-
-    if (!insertobservacion.command) {
-        var envio = { status: "No es posible ingresar una observacion con esos datos: " + insertobservacion };
-        return res.send(envio);
-    } else {
-        var resp = { status: true };
-        return res.send(resp);
+        if (!insertobservacion.command) {
+            var envio = { status: "No es posible ingresar una observacion con esos datos: " + insertobservacion };
+            return res.send(envio);
+        } else {
+            var resp = { status: true };
+            return res.send(resp);
+        }
+    }
+    else {
+        res.send({ status: "ese estudiante no tiene ese plan: "});
     }
 
-
 }
+
+
+
+
+
+
 
 
 module.exports = observacionesRouter;

77fd0a2bcc06c8d0592b3610fbbdf5b6050b9ac0
Jordan <jescarraga@unal.edu.co>
2022-05-31 21:30:58 -0500

    formato

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-var express = require('express');
-var path = require('path');
-var createError = require('http-errors');
+var express = require("express");
+var path = require("path");
+var createError = require("http-errors");
 //var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var logger = require("morgan");
+var cors = require("cors");
 
-
-var indexRouter = require('./routes/index');
-var docenteRouter = require('./routes/ingresoDocente');
-var bienestarRouter = require('./routes/bienestar');
-var ingresoEstudianteRouter = require('./routes/ingresoEstudiante');
-var ingresoTutorRouter = require('./routes/ingresoTutor');
-var observacionesRouter = require('./routes/observaciones');
-var remisionesDocenteRouter = require('./routes/remisionesDocente');
+var indexRouter = require("./routes/index");
+var docenteRouter = require("./routes/ingresoDocente");
+var bienestarRouter = require("./routes/bienestar");
+var ingresoEstudianteRouter = require("./routes/ingresoEstudiante");
+var ingresoTutorRouter = require("./routes/ingresoTutor");
+var observacionesRouter = require("./routes/observaciones");
+var remisionesDocenteRouter = require("./routes/remisionesDocente");
 
 var app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.use('/', indexRouter);
-app.use('/', ingresoEstudianteRouter);
-app.use('/', bienestarRouter);
-app.use('/',docenteRouter);
-app.use('/', ingresoTutorRouter);
-app.use('/', observacionesRouter);
-app.use('/', remisionesDocenteRouter);
-
+app.use("/", indexRouter);
+app.use("/", ingresoEstudianteRouter);
+app.use("/", bienestarRouter);
+app.use("/", docenteRouter);
+app.use("/", ingresoTutorRouter);
+app.use("/", observacionesRouter);
+app.use("/", remisionesDocenteRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -40,11 +37,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // app.listen( 3500, () =>{
--- routes/remisionesDocente.js
+++ routes/remisionesDocente.js
@@ -1,80 +1,83 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const queryCreator = require('../queryUtilities/queryCreator');
-const sendNull = require('../queryUtilities/queryNull');
+const express = require("express");
+const bodyParser = require("body-parser");
+const queryCreator = require("../queryUtilities/queryCreator");
+const sendNull = require("../queryUtilities/queryNull");
 
 const remisionesDocenteRouter = express.Router();
 remisionesDocenteRouter.use(bodyParser.json());
 
-remisionesDocenteRouter.route('/remisionesDocente')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        next();
-    })
-    .post(docenteInsertarRemision)
-    .get(obtenerTiposDeRemisiones)
-    .put(sendNull)
-    .delete(sendNull);
-
-
+remisionesDocenteRouter
+  .route("/remisionesDocente")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    next();
+  })
+  .post(docenteInsertarRemision)
+  .get(obtenerTiposDeRemisiones)
+  .put(sendNull)
+  .delete(sendNull);
 
 async function docenteInsertarRemision(req, res, next) {
-    var request = req.body;
-    var res1 = await queryCreator(`select codigo_plan FROM public.tutores where documento_estudiante = 
+  var request = req.body;
+  var res1 =
+    await queryCreator(`select codigo_plan FROM public.tutores where documento_estudiante = 
     '${request.documento_estudiante}' and documento_docente = '${request.documento_docente}';`);
-    var codigos_estudiante = res1.rows.map((r) => { return r.codigo_plan });
+  var codigos_estudiante = res1.rows.map((r) => {
+    return r.codigo_plan;
+  });
 
-    if (codigos_estudiante.includes(+request.codigo_plan)) {
-        var insertRemision = await queryCreator(
-            `CALL public.insert_remision(
+  if (codigos_estudiante.includes(+request.codigo_plan)) {
+    var insertRemision = await queryCreator(
+      `CALL public.insert_remision(
                 '${request.documento_docente}',
                 '${request.documento_estudiante}',
                 ${request.codigo_plan},
                 '${request.motivo_remision}',
                 ${request.codigo_tipo_remision});`
-        );
+    );
 
-        if (!insertRemision.command) {
-            var envio = { status: "No es posible ingresar una remision con esos datos: " + insertRemision };
-            return res.send(envio);
-        } else {
-            var resp = { status: true };
-            return res.send(resp);
-        }
+    if (!insertRemision.command) {
+      var envio = {
+        status:
+          "No es posible ingresar una remision con esos datos: " +
+          insertRemision,
+      };
+      return res.send(envio);
+    } else {
+      var resp = { status: true };
+      return res.send(resp);
     }
-    else {
-        res.send({ status: "ese estudiante no tiene ese plan asociado con ese docente" });
-    }
-
+  } else {
+    res.send({
+      status: "ese estudiante no tiene ese plan asociado con ese docente",
+    });
+  }
 }
 
 async function obtenerTiposDeRemisiones(req, res, next) {
-    var consulta = await queryCreator(`select * FROM public.tipo_remisiones;`);
-    var res1 = consulta.rows;
-    return res.send(res1);
+  var consulta = await queryCreator(`select * FROM public.tipo_remisiones;`);
+  var res1 = consulta.rows;
+  return res.send(res1);
 }
 
+remisionesDocenteRouter
+  .route("/remisionesDocente/estudiante")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    next();
+  })
+  .post(sendNull)
+  .get(docenteObtenerRemisionesEstudiante)
+  .put(sendNull)
+  .delete(sendNull);
 
-
-remisionesDocenteRouter.route('/remisionesDocente/estudiante')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        next();
-    })
-    .post(sendNull)
-    .get(docenteObtenerRemisionesEstudiante)
-    .put(sendNull)
-    .delete(sendNull);
-
-async function docenteObtenerRemisionesEstudiante(req, res, next){
-    var request = req.query;
-    var consulta = await queryCreator(`select * FROM public.remisiones where documento_docente like
+async function docenteObtenerRemisionesEstudiante(req, res, next) {
+  var request = req.query;
+  var consulta =
+    await queryCreator(`select * FROM public.remisiones where documento_docente like
     '${request.documentoDocente}' and documento_estudiante like '${request.documentoEstudiante}';`);
-    var res1 = consulta.rows;
-    return res.send(res1);
+  var res1 = consulta.rows;
+  return res.send(res1);
 }
-
-
-
 
 module.exports = remisionesDocenteRouter;

7b2896fc82354f750f07eba5659bc16c0920f03f
johanrodriguez1113 <johan.rodriguez@integrait.co>
2022-06-19 20:55:06 -0500

    Historial desde estudiante

--- routes/historialTutorias.js
+++ routes/historialTutorias.js
@@ -19,7 +19,7 @@ historialTutorias.route('/docente/tutorias/historial')
         estudiante = req.query.documentoEstudiante;
         docente = req.query.documentoDocente;
         if (docente) {
-            if (estudiante){
+            if (estudiante) {
                 consultaDocente = queryCreator(
                     `SELECT t.documento_docente, t.documento_estudiante, t.fecha_de_la_tutoria,
                                 t.estado_tutoria, u.nombres as nombre_estudiante, u.apellidos as apellido_estudiante 
@@ -29,8 +29,7 @@ historialTutorias.route('/docente/tutorias/historial')
                 ).then((result) => {
                     return res.json(result.rows);
                 })
-            }
-            else{
+            } else {
                 consultaDocente = queryCreator(
                     `SELECT t.documento_docente, t.documento_estudiante, t.fecha_de_la_tutoria,
                                 t.estado_tutoria, u.nombres as nombre_estudiante, u.apellidos as apellido_estudiante 
@@ -42,12 +41,36 @@ historialTutorias.route('/docente/tutorias/historial')
             }
 
         } else {
-            res.json({ message: "Invalid parameters" })
+            res.json({message: "Invalid parameters"})
         }
     })
     .post(sendNull)
     .put(sendNull)
     .delete(sendNull);
 
+historialTutorias.route('/estudiante/tutorias/historial')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        next();
+    })
+    .get((req, res, next) => {
+        estudiante = req.query.documentoEstudiante;
+        if (estudiante) {
+            consultaEstudiante = queryCreator(
+                `SELECT t.documento_docente, t.documento_estudiante, t.fecha_de_la_tutoria,
+                                t.estado_tutoria, u.nombres as nombre_docente, u.apellidos as apellido_docente 
+                                FROM tutorias t inner join usuario u on t.documento_docente = u.documento  WHERE
+                                documento_estudiante = '${estudiante}'
+                                 AND fecha_de_la_tutoria < CURRENT_DATE ;`
+            ).then((result) => {
+                return res.json(result.rows);
+            })
+        } else {
+            res.json({message: "Invalid parameters"})
+        }
+    })
+    .post(sendNull)
+    .put(sendNull)
+    .delete(sendNull);
 
 module.exports = historialTutorias;

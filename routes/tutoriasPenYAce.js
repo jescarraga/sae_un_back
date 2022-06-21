bfc05af966b5ea622b5d5427aa9143b914254668
Jordan <jescarraga@unal.edu.co>
2022-06-21 02:54:59 -0500

    get exitoso

--- routes/tutoriasPenYAce.js
+++ routes/tutoriasPenYAce.js
@@ -46,4 +46,34 @@ async function tutorias_pendientes(req, res, next) {
 }
 }
 
+async function tutorias_pendientes(req, res, next) {
+  var request = req.body;
+
+  if (request.id_tipo_usuario == '2') {
+    var obtener_tutorias_pendientes = await queryCreator(
+      `SELECT public.ver_tutorias_pendientes(
+                    ${request.id_tipo_usuario},
+                    '${request.documento}');`
+    );
+
+    listaTutoriasPendientes = [];
+
+    for (let index = 0; index <obtener_tutorias_pendientes.rows.length; index++) {
+      
+      var longitudCadena = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.length;
+      var tutoria_vector = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.substring(1,longitudCadena-1).split(',');
+      var tutoria = {
+        documento :tutoria_vector[0],
+        nombre: tutoria_vector[1].substring(1,tutoria_vector[1].length-1),
+        fecha_de_la_tutoria:  tutoria_vector[2]
+      };
+      
+      listaTutoriasPendientes[index]= tutoria;
+      
+    }
+
+    return res.send(listaTutoriasPendientes);
+}
+}
+
 module.exports = tutoriasPenYAceRouter;

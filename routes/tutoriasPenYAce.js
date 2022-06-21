9490e3b118c5d0c92e439a524fb2d258d0ed682f
Jordan <jescarraga@unal.edu.co>
2022-06-21 03:16:19 -0500

    Get y post terminados

--- routes/tutoriasPenYAce.js
+++ routes/tutoriasPenYAce.js
@@ -11,7 +11,7 @@ tutoriasPenYAceRouter.route('/tutoriasPenYAce')
     res.statusCode = 200;
     next();
   })
-  .post(sendNull)
+  .post(tutoria_realizada)
   .get(tutorias_pendientes)
   .put(sendNull)
   .delete(sendNull);
@@ -29,7 +29,6 @@ async function tutorias_pendientes(req, res, next) {
     listaTutoriasPendientes = [];
 
     for (let index = 0; index <obtener_tutorias_pendientes.rows.length; index++) {
-      
       var longitudCadena = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.length;
       var tutoria_vector = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.substring(1,longitudCadena-1).split(',');
       var tutoria = {
@@ -37,42 +36,27 @@ async function tutorias_pendientes(req, res, next) {
         nombre: tutoria_vector[1].substring(1,tutoria_vector[1].length-1),
         fecha_de_la_tutoria:  tutoria_vector[2]
       };
-      
       listaTutoriasPendientes[index]= tutoria;
-      
     }
-
     return res.send(listaTutoriasPendientes);
 }
 }
 
-async function tutorias_pendientes(req, res, next) {
+async function tutoria_realizada(req, res, next) {
   var request = req.body;
 
   if (request.id_tipo_usuario == '2') {
     var obtener_tutorias_pendientes = await queryCreator(
-      `SELECT public.ver_tutorias_pendientes(
-                    ${request.id_tipo_usuario},
-                    '${request.documento}');`
+      `UPDATE public.tutorias
+      SET estado_tutoria = 4
+      WHERE documento_docente='${request.documento_docente}' AND documento_estudiante='${request.documento_estudiante}' AND fecha_de_la_tutoria='${request.fecha_de_la_tutoria}';
+       `
     );
-
-    listaTutoriasPendientes = [];
-
-    for (let index = 0; index <obtener_tutorias_pendientes.rows.length; index++) {
-      
-      var longitudCadena = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.length;
-      var tutoria_vector = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.substring(1,longitudCadena-1).split(',');
-      var tutoria = {
-        documento :tutoria_vector[0],
-        nombre: tutoria_vector[1].substring(1,tutoria_vector[1].length-1),
-        fecha_de_la_tutoria:  tutoria_vector[2]
-      };
-      
-      listaTutoriasPendientes[index]= tutoria;
-      
+    if(!obtener_tutorias_pendientes){
+      return res.send({status : 'error'});
+    }else{
+      return res.send({status : 'ok'});
     }
-
-    return res.send(listaTutoriasPendientes);
 }
 }
 

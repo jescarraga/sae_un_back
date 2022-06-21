8e734f9f531abdaaa1f782639e755b496b877bc2
Jordan <jescarraga@unal.edu.co>
2022-06-21 14:59:20 -0500

    Estado agregado en el get

--- routes/tutoriasPenYAce.js
+++ routes/tutoriasPenYAce.js
@@ -21,25 +21,17 @@ async function tutorias_pendientes(req, res, next) {
 
   if (request.id_tipo_usuario == '1' || request.id_tipo_usuario == '2') {
     var obtener_tutorias_pendientes = await queryCreator(
-      `SELECT public.ver_tutorias_pendientes(
+      `SELECT * from public.ver_tutorias_pendientes(
                     ${request.id_tipo_usuario},
                     '${request.documento}');`
     );
 
-    listaTutoriasPendientes = [];
-
-    for (let index = 0; index <obtener_tutorias_pendientes.rows.length; index++) {
-      var longitudCadena = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.length;
-      var tutoria_vector = obtener_tutorias_pendientes.rows[index].ver_tutorias_pendientes.substring(1,longitudCadena-1).split(',');
-      var tutoria = {
-        documento :tutoria_vector[0],
-        nombre: tutoria_vector[1].substring(1,tutoria_vector[1].length-1),
-        fecha_de_la_tutoria:  tutoria_vector[2]
-      };
-      listaTutoriasPendientes[index]= tutoria;
-    }
-    return res.send(listaTutoriasPendientes);
-}
+    return res.send(obtener_tutorias_pendientes);
+  }if(!obtener_tutorias_pendientes){
+    return res.send({status : 'error'});
+  }else{
+    return res.send({status : 'ok'});
+  }
 }
 
 async function tutoria_realizada(req, res, next) {
